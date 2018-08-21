837915dbeada4ffc5b891b6e3ca2035db30e8e97
Les Stuck <les@lesstuck.com>
2018-08-21 01:42:35 -0700

    brought in xr-test files to naturalForces/nodeWorld

--- nodeWorld/app.js
+++ nodeWorld/app.js
@@ -1,4 +1,4 @@
-var express = require('express');
+    var express = require('express');
 var app = express();
 const server = app.listen(3000);
 var path = require("path");
---
+++ nodeWorld/app_old.js
@@ -0,0 +1,44 @@
+    var express = require('express');
+var app = express();
+const server = app.listen(3000);
+var path = require("path");
+const io = require("socket.io")(server);
+var mqtt = require("mqtt");
+var client = mqtt.connect("mqtt://127.0.0.1");
+client.on("connect", function() {
+    client.publish("tip");
+});
+var osc = require('osc');
+app.use(express.static('./public'));
+// app.get('/', function(req, res) {
+//     //res.sendFile(__dirname + '/index.html');
+//     res.sendFile(__dirname + '/index.html');
+// });
+// pass mqtt messages to socket
+// client.on("message", function(topic, message) {
+//     io.sockets.emit("data", String(message));
+//     console.log(String(message));
+// });
+
+var oscServer = new osc.UDPPort({
+  // localAddress: "192.168.1.2",
+  localAddress: "10.0.1.15",
+    localPort: 8000
+});
+oscServer.open();
+io.on("connection", function(socket) {
+    socket.on("newLoop", function(data) {
+      console.log("newLoop: " + data);
+    })
+});
+
+    // // OSC messages to mqtt to control motor
+    // client.publish("tip", String(messInt, 2));
+    // // OSC messages to socket to control webVR animations
+    // io.sockets.emit('data', mess);
+    // console.log("OSC Message: ", mess);
+    // // OSC messages to Mad Mapper
+    // oscServer.send({
+    //     address: '/tipper',
+    //     args: mess
+    // }, '127.0.0.1', 9000);
